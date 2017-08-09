bdd9576244bd4304d2746a42e1e623a158c96b98
ben scarborough <benscarborough@scarflix.com>
2017-08-09 11:30:32 -0400

    It 'works'

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,2 +1,34 @@
 class ShoesController < ApplicationController
+
+  def index
+    @store = Store.find(params[:store_id])
+    @shoes = @store.shoes
+  end
+
+  def new
+    @shoe = Shoe.new
+    @store = Store.find(params[:store_id])
+  end
+
+  def create
+    @shoe = Shoe.new(shoes_params)
+    @store = Store.find(params[:store_id])
+    @shoe.store_id = @store.id
+    if @shoe.save
+      redirect_to store_shoes_path(@store)
+    else
+      render('new')
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def shoes_params
+    params.require(:shoe).permit(:name, :color)
+  end
+
 end
